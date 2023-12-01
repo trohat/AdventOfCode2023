88a47e918cd8c48793cec6fe9c65058b88d2851e
Trohat <petr.sed@centrum.cz>
2023-12-01 07:01:00 +0100

    Template added

---
+++ template/task.py
@@ -0,0 +1,14 @@
+with open("input.txt") as infile:
+    data = infile.read()
+    
+data = data.split("\n")
+data = list(map(list, data))
+
+def task1(data):
+    return None
+
+def task2(data):
+    return None
+
+print(f"Task1: {task1(data)}")
+print(f"Task2: {task2(data)}")
