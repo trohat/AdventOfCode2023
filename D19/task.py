61386410f6cd9960b77195226dcd753af2cdc697
Trohat <petr.sed@centrum.cz>
2024-01-08 22:27:14 +0100

    Day 19

--- D19/task.py
+++ D19/task.py
@@ -1,5 +1,7 @@
 import re
 import sys
+from collections import deque
+import math
 
 file = "test.txt"
 if len(sys.argv) > 1:
@@ -42,7 +44,6 @@ def parse_data(data):
     return system, parts
 
 data = parse_data(data)
-print(data)
 
 def task1(system, parts):
     result = 0
@@ -67,8 +68,99 @@ def task1(system, parts):
 
 def task2(system):
     result = 0
-
-
+    to_process = deque()
+    to_process.append({
+        "ranges": {
+            "x": range(1, 4001),
+            "m": range(1, 4001),
+            "a": range(1, 4001),
+            "s": range(1, 4001)
+        },
+        "workflow": "in"
+    })
+    while len(to_process) > 0:
+        part = to_process.popleft()
+        ranges = part["ranges"]
+        workflow = part["workflow"]
+        if workflow == "R":
+            continue
+        if workflow == "A":
+            result += math.prod(map(len, ranges.values()))
+            continue
+        workflow = system[workflow]
+        for rule in workflow:
+            if "final_target" in rule:
+                to_process.append({
+                    "ranges": ranges,
+                    "workflow": rule["final_target"]
+                })
+                break
+            category = rule["category"]    
+            gt = rule["gt"]    
+            boundary = rule["boundary"]    
+            target_rule = rule["target"]
+            target_range = ranges[category]
+            if boundary < target_range.start:
+                if gt:
+                    to_process.append({
+                        "ranges": ranges,
+                        "workflow": target_rule
+                    })
+                    break
+                else:
+                    continue
+            if boundary >= target_range.stop:
+                if not gt:
+                    to_process.append({
+                        "ranges": ranges,
+                        "workflow": target_rule
+                    })
+                    break
+                else:
+                    continue
+            if boundary in target_range:
+                if gt:
+                    new_low_range = range(target_range.start, boundary + 1)
+                    new_high_range = range(boundary + 1, target_range.stop)
+                    if new_high_range:
+                        new_ranges = ranges.copy()
+                        new_ranges.update({
+                                category: new_high_range
+                            })
+                        to_process.append({
+                            "ranges": new_ranges,
+                            "workflow": target_rule
+                        })
+                    if new_low_range: # always true
+                        ranges.update({
+                            category: new_low_range
+                        })
+                        continue
+                    else:
+                        print("gt, should be never printed")
+                        break
+                else:
+                    new_low_range = range(target_range.start, boundary)
+                    new_high_range = range(boundary, target_range.stop)
+                    if new_low_range:
+                        new_ranges = ranges.copy()
+                        new_ranges.update({
+                                category: new_low_range
+                            })
+                        to_process.append({
+                            "ranges": new_ranges,
+                            "workflow": target_rule
+                        })
+                    if new_high_range: # always true
+                        ranges.update({
+                            category: new_high_range
+                        })
+                        continue
+                    else:
+                        print("not gt, should be never printed")
+                        break
+            print("Failed.")
+            exit(0)
     return result
 
 print(f"Task1: {task1(*data)}")
